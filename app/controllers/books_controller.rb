dc1c2628ae4a398daa78b7ffbc0e21b541697f00
AyumiShiiba <175018766+kinakonomoti@users.noreply.github.com>
2024-10-06 08:41:43 +0000

    [Finish]2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,22 @@
 class BooksController < ApplicationController
-  def new
-  end
-
+  
   def index
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:new, :indx, :show]
+  resources :books, only: [:index, :create, :show]
   get 'homes/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
