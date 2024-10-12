75725ae8def1861d824100ea9760f22110f0d776
AyumiShiiba <175018766+kinakonomoti@users.noreply.github.com>
2024-10-12 07:06:32 +0000

    [Finish]3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,29 @@
 class BooksController < ApplicationController
 
-def index
-  @book = Book.new
-end
-
 def create
   @book = Book.new(book_params)
   @book.user_id = current_user.id
   if @book.save
-    redirect_to book_path(@book)
+    redirect_to book_path(@book.id)
   else
+    @book = Book.all
     render :index
   end
 end
 
+def index
+    @books = Book.all
+    @book = Book.new
+end
+
 def show
-  
+  @book = Book.find(params[:id])
+end
+
+def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
 end
 
 private
